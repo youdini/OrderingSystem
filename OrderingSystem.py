bba3249d8134b844be172f7e51287023d880c158
youdini <johnlee3156@gmail.com>
2022-11-03 17:02:13 +0800

    modified:   OrderingSystem.py

--- OrderingSystem.py
+++ OrderingSystem.py
@@ -2,6 +2,12 @@ from textual.app import App, ComposeResult
 from textual.widgets import Button, Static, Footer
 from textual.containers import Container, Horizontal, Vertical
 
+class Menu(Static):
+    
+    def compose(self) -> ComposeResult:
+        yield Static("Menu")
+
+
 class AbdulApp(App):
 
     def compose(self) -> ComposeResult:
