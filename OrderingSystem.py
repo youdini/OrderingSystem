579144aecb1357d4cb5aa35c86f6a1c33aed4a08
youdini <95413220+youdini@users.noreply.github.com>
2022-11-07 21:42:16 +0800

    add 3rd button

--- OrderingSystem.py
+++ OrderingSystem.py
@@ -5,15 +5,22 @@ from textual.containers import Container, Horizontal, Vertical
 class Menu(Static):
     
     def compose(self) -> ComposeResult:
-        yield Static("Menu")
+        yield Button("Menu")
 
 
 class AbdulApp(App):
 
     def compose(self) -> ComposeResult:
         yield Footer()
-        yield Button("Hi")
-        yield Static("New changes")
+        yield Container (
+            Button("Hello"),
+            Horizontal(
+                Button("1st Button"),
+                Button("2nd Button"),
+                Button("3rd Button")
+            ),
+            id="container"
+        )
         
 if __name__ == "__main__":
     AbdulApp().run()
